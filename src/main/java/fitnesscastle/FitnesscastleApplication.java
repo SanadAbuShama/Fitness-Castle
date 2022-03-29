fb194c058183b4e9b98e37226096f4cd3cd9cc0d
Sanad Abu Shama <abushamasanad@hotmail.com>
2022-03-29 10:32:59 +0300

    First Commit

---
+++ src/main/java/fitnesscastle/FitnesscastleApplication.java
@@ -0,0 +1,13 @@
+package fitnesscastle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesscastleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnesscastleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fitnesscastle/FitnesscastleApplicationTests.java
@@ -0,0 +1,13 @@
+package fitnesscastle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesscastleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
